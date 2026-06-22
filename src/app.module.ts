7a68d19e1cececccba7b87592fa72c8732b988ec
leehyunmin <leehm2292@gmail.com>
2026-06-22 14:14:36 +0900

    chore: postgresql DB 연결 및 users 엔티티 정의

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,34 @@ import { ReturnRequestsModule } from './return-requests/return-requests.module';
 import { StayStatusModule } from './stay-status/stay-status.module';
 import { MeritLogsModule } from './merit-logs/merit-logs.module';
 import { ScheduleModule } from './schedule/schedule.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, NoticesModule, SuggestionsModule, LaundryModule, CleaningDutyModule, MorningSongsModule, ReturnRequestsModule, StayStatusModule, MeritLogsModule, ScheduleModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    NoticesModule,
+    SuggestionsModule,
+    LaundryModule,
+    CleaningDutyModule,
+    MorningSongsModule,
+    ReturnRequestsModule,
+    StayStatusModule,
+    MeritLogsModule,
+    ScheduleModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
