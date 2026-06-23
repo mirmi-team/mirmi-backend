aa06441bb54eb0a7cb716ef53b6537d2a9fe70d6
leehyunmin <leehm2292@gmail.com>
2026-06-23 21:01:47 +0900

    #12 fix: 학교 도메인만 설정

--- src/auth/dto/send-email.dto.ts
+++ src/auth/dto/send-email.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, Matches } from 'class-validator';
 
 export class SendEmailDto {
   @IsEmail()
-  @Matches(/@e-mirim\.hs\.kr$/, { // 정규식
+  @Matches(/@e-mirim\.hs\.kr$/, {
     message: '학교 이메일로만 가입할 수 있습니다.',
   })
   email: string;
