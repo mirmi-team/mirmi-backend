29a38f3f97341e8a61d8c4c24c6a054da455ac77
leehyunmin <leehm2292@gmail.com>
2026-06-26 01:29:49 +0900

    #25 feat: 이메일 발송자 이름 형식 변경

--- src/auth/mail.service.ts
+++ src/auth/mail.service.ts
@@ -13,9 +13,9 @@ export class MailService {
 
   async sendVerificationCode(to: string, code: string) {
     await this.transporter.sendMail({
-      from: process.env.MAIL_USER,
+      from: `"Mirmi" <${process.env.MAIL_USER}>`,
       to,
-      subject: '[기숙사앱] 이메일 인증번호',
+      subject: '[Mirmi] 이메일 인증번호',
       text: `인증번호는 ${code} 입니다. 5분 안에 입력해주세요.`,
     });
   }
