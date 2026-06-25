6bfc6add6e6f8fef3afe6127a1c7620a75b87186
leehyunmin <leehm2292@gmail.com>
2026-06-25 10:42:43 +0900

    #21 feat: 오늘 공지사항 사용자  조회, 관리자 CRUD 구현

--- src/notices/dto/create-notice.dto.ts
+++ src/notices/dto/create-notice.dto.ts
@@ -1,1 +1,11 @@
-export class CreateNoticeDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateNoticeDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
--- src/notices/notices.controller.ts
+++ src/notices/notices.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { NoticesService } from './notices.service';
 import { CreateNoticeDto } from './dto/create-notice.dto';
 import { UpdateNoticeDto } from './dto/update-notice.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('notices')
 export class NoticesController {
   constructor(private readonly noticesService: NoticesService) {}
 
+  // 오늘 공지사항 조회 - 누구나 가능
+  @Get('findOne')
+  findOne() {
+    return this.noticesService.findOne();
+  }
+
+  //등록 - 관리자만
   @Post()
-  create(@Body() createNoticeDto: CreateNoticeDto) {
-    return this.noticesService.create(createNoticeDto);
+  @UseGuards(JwtAuthGuard, RolesGuard) // 토큰 검증 -> 권한 검증
+  @Roles('ADMIN')
+  create(@Body() dto: CreateNoticeDto) {
+    return this.noticesService.create(dto);
   }
 
-  @Get()
-  findAll() {
-    return this.noticesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.noticesService.findOne(+id);
-  }
-
+  //수정 - 관리자만
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoticeDto: UpdateNoticeDto) {
-    return this.noticesService.update(+id, updateNoticeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.noticesService.remove(+id);
+  @UseGuards(JwtAuthGuard, RolesGuard) // 토큰 검증 -> 권한 검증
+  @Roles('ADMIN')
+  update(@Param('id') id: string, @Body() dto: UpdateNoticeDto) {
+    return this.noticesService.update(+id, dto);
   }
 }
