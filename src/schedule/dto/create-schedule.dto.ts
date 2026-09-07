d7719ee0a206922b5bd78ee0a2eea3355c2a0da0
2xazun <ha.jun.did.it@gmail.com>
2026-09-07 11:18:47 +0900

    feat: 기숙사 일정 조회/등록/수정/삭제 API 구현

--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,1 +1,20 @@
-export class CreateScheduleDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateScheduleDto {
+  @ApiProperty({
+    description: '일정 제목',
+    example: '기숙사 점호 시간 변경',
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    description: '일정 설명',
+    example: '시험 기간으로 인해 점호 시간이 23시로 변경됩니다.',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
