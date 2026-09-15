3ecec398c2a5a6b84b2c698a579f6bb0eda8504f
leehyunmin <leehm2292@gmail.com>
2026-09-15 09:31:40 +0900

    #95 feat: 이메일 발송 테스트 코드에서 실제로 API 요청하는 부분을 요청 안하도록 수정

--- test/utils/e2e-setup.ts
+++ test/utils/e2e-setup.ts
@@ -1,3 +1,10 @@
+// E2E 테스트에서는 실제 SendGrid API를 호출하지 않도록 Mock 처리한다.
+// (SendGrid 무료 한도 초과로 인한 401 에러 방지)
+jest.mock('@sendgrid/mail', () => ({
+  setApiKey: jest.fn(),
+  send: jest.fn().mockResolvedValue([{ statusCode: 202 }]),
+}));
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
