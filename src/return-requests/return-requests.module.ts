94d65927939329881cee0742d3741772c1afcd2e
2xazun <ha.jun.did.it@gmail.com>
2026-09-24 12:43:01 +0900

    feat: 관리자용 층별 전체학생 오늘 복귀 현황 조회 API 추가

--- src/return-requests/return-requests.module.ts
+++ src/return-requests/return-requests.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReturnRequestsService } from './return-requests.service';
 import { ReturnRequestsController } from './return-requests.controller';
 import { ReturnRequestsAdminController } from './return-requests-admin.controller';
 import { SupabaseModule } from 'src/common/supabase/supabase.module';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, TypeOrmModule.forFeature([User])],
   controllers: [ReturnRequestsController, ReturnRequestsAdminController],
   providers: [ReturnRequestsService],
   exports: [ReturnRequestsService],
